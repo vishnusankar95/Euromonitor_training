d260797141b50efe58b78185121c3724508a02f4
Vishnu Sankar <Vishnu.Sankar@euromonitor.com>
2022-04-07 17:03:10 +0530

    Sccount creation

--- Taks1/index.js
+++ Taks1/index.js
@@ -13,8 +13,10 @@ function toggle_Account_Type(){
     let acc_type = document.getElementById("account_Type").value;
     console.log("styes : ",selectData.style.display)
     console.log("Selected item is :", acc_type)
-
-    selectData.style.display = 'block'
+    if(acc_type== "savings" || acc_type == "current")
+    selectData.style.display = 'block';
+    else
+    selectData.style.display = 'none';
 }
 
 
@@ -26,15 +28,65 @@ function valiteForm(){
   let country = document.getElementById("country").value;
   let email = document.getElementById("email").value;
   let acc_type = document.getElementById("account_Type").value;
+  let acc_nm = ( Math.random().toString().substring(2, 14));
+  let acc_num = "";
+  let submitOk = true;
+  let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-  if(age>68){
-    alert("You are not eligible for account opening");
+  //validators
+
+  if(name.length <1 || location.length <1 || state.length <1 || country.length <1){
+    submitOk = false;
   }
   
+  if(age>68){
+    alert("You are not eligible for account opening");
+    submitOk = false;
+  }
 
-  let userdata = {acc_type,name,age,location,state,country,email}
+  if(!ValidateEmail(email)){
+    submitOk = false;
+  }
 
-  let cusomer_Data = localStorage.setItem("userdata",JSON.stringify(userdata));
+  //Account number Creator
+
+  if(acc_type == "savings"){
+    acc_num = "Sav"+acc_nm.toString();
+  }
+  else if(acc_type == "current"){
+    acc_num = "Curr"+acc_nm.toString();
+  }
+
+  //form handle 
+
+  if(submitOk==false){
+    alert("Please fill with valid details.");
+    return false;
+  }
+  else{
+
+    let userdata = {acc_type,name,age,location,state,country,email,acc_num}
+
+    let cusomer_Data = localStorage.setItem("userdata",JSON.stringify(userdata));
+
+    pageshifter()
+    return true
+  }
   
-  window.location.href="./accountdata.html";
+}
+
+function ValidateEmail(email) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(custmmer_Data.email.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+
+
+function pageshifter(){
+  console.log(1234)
+window.location.href="./accountdata.html";
 }
