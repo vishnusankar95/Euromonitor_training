2f052cf9c3821f2dc848648aff871f0df42546e2
Vishnu Sankar <Vishnu.Sankar@euromonitor.com>
2022-07-05 10:27:31 +0530

    Landing page Design

--- onetracker/src/app/app.module.ts
+++ onetracker/src/app/app.module.ts
@@ -1,27 +1,37 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HomeTrackerComponent } from './components/home-tracker/home-tracker.component';
 import { LoginComponent } from './components/login/login.component';
+import {MatButtonModule} from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
+import {MatSelectModule} from '@angular/material/select'
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { SignupComponent } from './components/signup/signup.component';
-
+//import { TicketDataComponent } from './ticket-data/ticket-data.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
     SignupComponent,
-    HomeTrackerComponent
+    HomeTrackerComponent,
+    //TicketDataComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- onetracker/src/app/components/home-tracker/home-tracker.component.ts
+++ onetracker/src/app/components/home-tracker/home-tracker.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface daysfilter {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-home-tracker',
   templateUrl: './home-tracker.component.html',
   styleUrls: ['./home-tracker.component.css']
 })
-export class HomeTrackerComponent implements OnInit {
 
+export class HomeTrackerComponent implements OnInit {
+  selectedValue?: string ;
   constructor() { }
 
+  daysFilters: daysfilter[] = [
+    {value: 'Last 3 days', viewValue: '3days'},
+    {value: 'Last 2 Days', viewValue: '2days'},
+    {value: 'Latest', viewValue: 'Latest'},
+  ];
+
+  
   ngOnInit(): void {
   }
-
 }
